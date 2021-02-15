65ceeefb6ce80405e0e212f692267a1102330464
nakamura ryo <60230025+knr2@users.noreply.github.com>
2021-02-16 01:56:17 +0900

    Update reading_rgb_from_picture.cpp

--- drawlib/improved_version/reading_rgb_from_picture.cpp
+++ drawlib/improved_version/reading_rgb_from_picture.cpp
@@ -9,8 +9,8 @@
 #define G_MAX 255
 #define B_MIN 0
 #define B_MAX 255
-#define height_MAX 400
-#define width_MAX 200
+#define height_MAX 500
+#define width_MAX 500
 
 int main(int argc, char *argv[])
 {
@@ -21,24 +21,34 @@ int main(int argc, char *argv[])
         exit(1);
     }
 
-    /*
-        // 同一フィルにある場合(１つの画像のみ)
-        // string input_filename = "original.png";
-    */
+    // 同一フィルにある場合(１つの画像のみ)
+    
+    std::string input_file = "train_arashiyama_torokko_kouyou.png";
+    cv::Mat input_image_rgb = cv::imread(input_file, CV_LOAD_IMAGE_COLOR);
+    
+    // RGBを保存
+    for(int height = 0;height < height_MAX;height++)
+    {
+        for(int width = 0;width < width_MAX;width++)
+        {
+            cv::Vec3b bgr = input_image_rgb.at<cv::Vec3b>(cv::Point(height, width));
+            fprintf(rgb, "%d\t%d\t%d\t%d\t%d\n", height, width, (int)bgr[2], (int)bgr[1], (int)bgr[0]);
+            std::cout << (int)bgr[0] << " " << (int)bgr[1] << " " << (int)bgr[2] << std::endl;
+        }
+    }
 
+    /*
     // 別ファイルにある場合(複数可能)
 
     // パスを指定(拡張子は除く)
     const std::string base = "/home/nakamura/Git/programming_base/drawlib/original";
     std::vector<std::string> files;
+
     // 使用されている拡張子を1つ指定
-    /*
     files.push_back(base + ".bmp"); // Windows bitmaps (bmp, dib)
     files.push_back(base + ".jpg"); // JPEG files (jpg, jpeg, jpe)
     files.push_back(base + ".jp2"); // JPEG 2000 files (jp2)
-    */
     files.push_back(base + ".png"); // Portable Network Graphics (png)
-    /*
     files.push_back(base + ".pbm"); // Portable image format (pbm:raw)
     files.push_back(base + "_ascii" + ".pbm"); // Portable image format (pbm:ascii)
     files.push_back(base + ".pgm"); // Portable image format (pgm:raw)
@@ -47,14 +57,11 @@ int main(int argc, char *argv[])
     files.push_back(base + "_ascii" + ".ppm"); // Portable image format (ppm:ascii)
     files.push_back(base + ".ras"); // Sun rasters (ras, sr)
     files.push_back(base + ".tiff"); // TIFF files (tiff, tif)
-    */
 
     // 表示するウィンドウ名を指定
     cv::namedWindow("image1", CV_WINDOW_AUTOSIZE|CV_WINDOW_FREERATIO);
-    /*
     cv::namedWindow("image2", CV_WINDOW_AUTOSIZE|CV_WINDOW_FREERATIO);
     cv::namedWindow("image3", CV_WINDOW_AUTOSIZE|CV_WINDOW_FREERATIO);
-    */
     
     std::vector<std::string>::iterator input_filename = files.begin();
 
@@ -63,12 +70,10 @@ int main(int argc, char *argv[])
         std::cout << *input_filename << std::endl;
         // 画像を3チャンネル(BGR)で読み込む
         cv::Mat input_image_rgb = cv::imread(*input_filename, CV_LOAD_IMAGE_COLOR);
-        /*
         // グレースケール画像として読み込む．
         cv::Mat img2 = cv::imread(*input_filename, 0);
         // 画像をそのまま読み込む．ただし，アルファチャンネルは無視される．
         cv::Mat img3 = cv::imread(*input_filename, -1);
-        */
 
         // RGBを保存
         for(int height = 0;height < height_MAX;height++)
@@ -83,13 +88,12 @@ int main(int argc, char *argv[])
         
         // 表示するウィンドウ数を指定 
         cv::imshow("image1", input_image_rgb);
-        /*
         cv::imshow("image2", img2);
         cv::imshow("image3", img3);
-        */
 
         if(cv::waitKey(0)==27) break;
     }
+    */
 
     fclose(rgb);
 }
